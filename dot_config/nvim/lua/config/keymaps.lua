190820bfba632aaafbf943f857884a2a80229d56
Tristan de Blauwe <tristan.deblauwe@cea.fr>
2026-02-26 07:27:01 +0100

    Add keymaps to navigate between quickfiles

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -19,6 +19,10 @@ vim.keymap.set("n", "<C-j>", "<C-w>J", { desc = "Move Split Down" })
 vim.keymap.set("n", "<C-k>", "<C-w>K", { desc = "Move Split Up" })
 vim.keymap.set("n", "<C-l>", "<C-w>L", { desc = "Move Split Right" })
 
+-- Navigate quickfix items easily
+vim.keymap.set("n", "<leader>n", ":cnext<CR>", { desc = "Next selected file" })
+vim.keymap.set("n", "<leader>p", ":cprev<CR>", { desc = "Previous selected file" })
+vim.keymap.set("n", "<leader>x", ":bp | sp | bn | bd<CR>:cnext<CR>", { desc = "Close and open next file" })
 
 --------------------------------------------------------------------------------
 --- CODE
